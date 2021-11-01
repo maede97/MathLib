bac0ae38798d45e3a2a4acc0cd207eb31f9be02d
maede97 <maede97@users.noreply.github.com>
2021-11-01 09:26:36 +0000

    deploy: 2587f2e7c023083494e6a780a8b0d696106a83e0

--- search/all_9.js
+++ search/all_9.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['tail_0',['tail',['../classVector.html#a2845a526b73aca5f9afbd195130247bc',1,'Vector']]],
-  ['type_1',['type',['../classVector.html#a9cf78ae1e922a9028ff3e423c8ffbc10',1,'Vector']]]
+  ['to_5fstring_1',['to_string',['../classVector.html#abe4dfd337f1d427866b45353806c77e4',1,'Vector']]],
+  ['type_2',['type',['../classVector.html#a9cf78ae1e922a9028ff3e423c8ffbc10',1,'Vector']]]
 ];
--- search/functions_8.js
+++ search/functions_8.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['tail_0',['tail',['../classVector.html#a2845a526b73aca5f9afbd195130247bc',1,'Vector']]]
+  ['tail_0',['tail',['../classVector.html#a2845a526b73aca5f9afbd195130247bc',1,'Vector']]],
+  ['to_5fstring_1',['to_string',['../classVector.html#abe4dfd337f1d427866b45353806c77e4',1,'Vector']]]
 ];
