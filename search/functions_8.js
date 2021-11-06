ad2be9289906b5401881baf73f52e1e4f81c7df2
maede97 <maede97@users.noreply.github.com>
2021-11-06 12:42:15 +0000

    deploy: e71fbcdc715e05218cf72213efaa979108c359b9

--- search/functions_8.js
+++ search/functions_8.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['quaternion_0',['Quaternion',['../classQuaternion.html#a6839e6bebb27d00b37442f8e56a92908',1,'Quaternion::Quaternion(T x, T y, T z, T w)'],['../classQuaternion.html#a9431dc17ca6305db22e9f32fc8836b97',1,'Quaternion::Quaternion(const Vector3_t &amp;axis, T angle)'],['../classQuaternion.html#afb3377af8c17de9ddc9f940db7e25a01',1,'Quaternion::Quaternion()=default'],['../classQuaternion.html#abb36776d231015a7300aaac28021a1f6',1,'Quaternion::Quaternion(const Quaternion &amp;other)']]]
+  ['quaternion_0',['Quaternion',['../classQuaternion.html#a6839e6bebb27d00b37442f8e56a92908',1,'Quaternion::Quaternion(T x, T y, T z, T w)'],['../classQuaternion.html#a9431dc17ca6305db22e9f32fc8836b97',1,'Quaternion::Quaternion(const Vector3_t &amp;axis, T angle)'],['../classQuaternion.html#afb3377af8c17de9ddc9f940db7e25a01',1,'Quaternion::Quaternion()=default'],['../classQuaternion.html#abb36776d231015a7300aaac28021a1f6',1,'Quaternion::Quaternion(const Quaternion &amp;other)'],['../classQuaternion.html#ac9d98b6c62e1f8f532d629d1f754e3e3',1,'Quaternion::Quaternion(const Vector3_t &amp;a, const Vector3_t &amp;b)']]]
 ];
