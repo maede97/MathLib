4409badac2d0c0c3598eb1992db16fd153cb18f9
maede97 <maede97@users.noreply.github.com>
2021-11-06 12:46:17 +0000

    deploy: 01b8220d0a3d0367e22136c498c38cf46e92be9e

--- search/functions_1.js
+++ search/functions_1.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['cast_0',['cast',['../classVector.html#a2bea1726c0edcb494e81608b7bc376f2',1,'Vector']]],
   ['commaloader_1',['CommaLoader',['../structVector_1_1CommaLoader.html#a189b826d05eefccb650c73bd980f9f14',1,'Vector::CommaLoader']]],
-  ['cross_2',['cross',['../classVector.html#abf5c716e89b902950bffaffd68f3f7eb',1,'Vector']]]
+  ['cross_2',['cross',['../classVector.html#ad6a74ed7ffaf0f772e53a4a10c87c136',1,'Vector']]]
 ];
